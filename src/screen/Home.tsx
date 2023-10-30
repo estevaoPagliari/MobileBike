7939eae708f51b1c314806f7191536dbd70eefb4
estevaoPagliari <estevao_pagliari@hotmail.com>
2023-10-30 07:56:16 -0300

    atualização

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -7,9 +7,16 @@ import {
   TouchableOpacity,
   View,
   Image,
+  TextInput,
+  Button,
 } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
-import { Entypo, Ionicons } from '@expo/vector-icons'
+import {
+  Entypo,
+  Ionicons,
+  MaterialCommunityIcons,
+  Feather,
+} from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
 import { propsStack } from '../routes/model'
 import { LinearGradient } from 'expo-linear-gradient'
@@ -17,6 +24,8 @@ import { api } from '../api/api'
 import jwtDecode from 'jwt-decode'
 import User from '../api/User'
 import Bike from '../api/Bike'
+import AnimateNumber from 'react-native-animate-number'
+import Modal from 'react-native-modal'
 
 interface DecodedToken {
   sub: string // Altere o tipo conforme a estrutura do seu token
@@ -26,8 +35,17 @@ export default function Home() {
   const [user, setUser] = useState<User | null>(null)
   const [bikes, setBike] = useState<Bike | null>(null)
   const [isLoading, setIsLoading] = useState(false) // Estado para controlar o carregamento
-  const [isLoadingBike, setIsLoadingBike] = useState(false) // Estado para controlar o carregamento
+  const [IsLightColor, setIsLightColor] = useState('black')
+  const [IsBlockColor, setIsBlockColor] = useState('black')
+  const [IsBlockIcon, setIsBlockIcon] = useState<string>('lock')
+  const [IsBleColor, setIsBleColor] = useState('black')
+  const [isModalVisible, setModalVisible] = useState(false)
+
   const navegation = useNavigation<propsStack>()
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible)
+  }
   async function signOut() {
     const signOutFunc = await SecureStore.deleteItemAsync('token')
     navegation.navigate('Welcome')
@@ -58,9 +76,32 @@ export default function Home() {
       })
       console.log('RETURN BIKE', response.data[0])
       setBike(response.data[0])
-      setIsLoadingBike(true)
     } catch (error) {
       console.error('Erro na solicitação GET:', error)
+    }
+  }
+
+  function Light(IsLightColor: string) {
+    if (IsLightColor === 'black') {
+      setIsLightColor('white')
+    } else {
+      setIsLightColor('black')
+    }
+  }
+  function Block(IsBlockColor: string) {
+    if (IsBlockColor === 'black') {
+      setIsBlockColor('white')
+      setIsBlockIcon('lock-open')
+    } else {
+      setIsBlockColor('black')
+      setIsBlockIcon('lock')
+    }
+  }
+  function Ble(IsBleColor: string) {
+    if (IsBleColor === 'black') {
+      setIsBleColor('white')
+    } else {
+      setIsBleColor('black')
     }
   }
 
@@ -69,7 +110,15 @@ export default function Home() {
     setTimeout(() => {
       loadUser()
       loadBike()
-    }, 8000)
+    }, 3000)
+
+    const intervalo = 60000
+
+    const intervalId = setInterval(() => loadBike(), intervalo)
+
+    return () => {
+      clearInterval(intervalId)
+    }
   }, [])
 
   return (
@@ -92,91 +141,299 @@ export default function Home() {
           {/** Fim Container Header */}
 
           <View style={styles.containermiddle}>
-            <View style={styles.containerbike}>
-              <LinearGradient
-                colors={['#D7EDEB', '#CDE9E6', '#9CD3CE', '#83C8C2']}
-                start={{ x: 0, y: 0.5 }} // Cores do gradiente (você pode adicionar quantas cores quiser)
-                style={{ borderRadius: 10 }}
-              >
-                <View style={styles.containerbiketitle}>
-                  {user.bike.map((bike) => (
-                    <Text
-                      key={bike.id}
-                      style={{
-                        fontSize: 25,
-                        fontWeight: 'bold',
-                        color: '#2E2E2E',
-                      }}
-                    >
-                      {bike.name}
-                    </Text>
-                  ))}
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'space-between',
-                    }}
+            {bikes ? (
+              <View>
+                <Text>Bike Cadastrada</Text>
+                <View style={styles.containerbike}>
+                  <LinearGradient
+                    colors={['#D7EDEB', '#CDE9E6', '#9CD3CE', '#83C8C2']}
+                    start={{ x: 0, y: 0.5 }} // Cores do gradiente (você pode adicionar quantas cores quiser)
+                    style={{ borderRadius: 10 }}
                   >
-                    <Text style={{ fontSize: 19, fontWeight: 'bold' }}>
-                      Conectado
-                    </Text>
-                    <Entypo
-                      style={{ left: 3, top: 2 }}
-                      name="controller-record"
-                      size={24}
-                      color="#ff0000"
+                    <View style={styles.containerbiketitle}>
+                      {user.bike.map((bike) => (
+                        <Text
+                          key={bike.id}
+                          style={{
+                            fontSize: 25,
+                            fontWeight: 'bold',
+                            color: '#2E2E2E',
+                          }}
+                        >
+                          {bike.name}
+                        </Text>
+                      ))}
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <Text style={{ fontSize: 19, fontWeight: 'bold' }}>
+                          Conectado
+                        </Text>
+                        <Entypo
+                          style={{ left: 3, top: 2 }}
+                          name="controller-record"
+                          size={24}
+                          color="#ff0000"
+                        />
+                      </View>
+                    </View>
+                  </LinearGradient>
+
+                  <View style={styles.containerbikestatus}>
+                    <Image
+                      style={styles.imagenbike}
+                      resizeMode="center"
+                      source={require('../assets/bike_teste.png')}
                     />
+                    <View style={styles.containerbiketext}>
+                      <View style={styles.containerbattery}>
+                        <Entypo name="battery" size={50} color="#0D9488" />
+                        {bikes ? (
+                          <View>
+                            {bikes.status.map((statusbike, index) => (
+                              <AnimateNumber
+                                value={statusbike.battery}
+                                key={index}
+                                style={styles.textbikeinfo}
+                                formatter={(val) => {
+                                  return parseFloat(val).toFixed(0)
+                                }}
+                              />
+                            ))}
+                          </View>
+                        ) : (
+                          <ActivityIndicator size="large" color="#0D9488" />
+                        )}
+
+                        <Text style={{ fontSize: 23, left: 0, top: 3 }}>
+                          {' '}
+                          %
+                        </Text>
+                      </View>
+                      <View style={styles.containerbattery}>
+                        <Ionicons
+                          name="speedometer-outline"
+                          size={50}
+                          color="#0D9488"
+                        />
+                        {bikes ? (
+                          <View>
+                            {bikes.status.map((statusbike, index) => (
+                              <Text key={index} style={styles.textbikeinfo}>
+                                {statusbike.velocity}
+                              </Text>
+                            ))}
+                          </View>
+                        ) : (
+                          <ActivityIndicator size="large" color="#0D9488" />
+                        )}
+
+                        <Text style={{ fontSize: 23, left: 0, top: 3 }}>
+                          {' '}
+                          Km
+                        </Text>
+                      </View>
+                    </View>
                   </View>
                 </View>
-              </LinearGradient>
-
-              <View style={styles.containerbikestatus}>
-                <Image
-                  style={styles.imagenbike}
-                  resizeMode="center"
-                  source={require('../assets/bike_teste.png')}
-                />
-                <View style={styles.containerbiketext}>
-                  <View style={styles.containerbattery}>
-                    <Entypo name="battery" size={50} color="#0D9488" />
-                    {bikes ? (
-                      <View>
-                        {bikes.status.map((statusbike, index) => (
-                          <Text key={index} style={styles.textbikeinfo}>
-                            {statusbike.battery}
-                          </Text>
-                        ))}
-                      </View>
-                    ) : (
-                      <ActivityIndicator size="large" color="#0D9488" />
-                    )}
-
-                    <Text style={{ fontSize: 23, left: 0, top: 3 }}> %</Text>
+                <View style={styles.containerPainel}>
+                  <Text
+                    style={{
+                      fontSize: 25,
+                      fontWeight: 'bold',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'space-around',
+                      padding: 5,
+                    }}
+                  >
+                    Painel
+                  </Text>
+                  <View style={styles.containerPainelItens}>
+                    <TouchableOpacity
+                      style={styles.ItensPainel}
+                      onPress={() => Light(IsLightColor)}
+                    >
+                      <Text
+                        style={{
+                          fontWeight: 'bold',
+                          fontSize: 17,
+                          color: IsLightColor,
+                        }}
+                      >
+                        Laterna
+                      </Text>
+                      <MaterialCommunityIcons
+                        name="car-light-high"
+                        size={60}
+                        color={IsLightColor}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.ItensPainel}
+                      onPress={() => Block(IsBlockColor)}
+                    >
+                      <Text
+                        style={{
+                          fontWeight: 'bold',
+                          fontSize: 17,
+                          color: IsBlockColor,
+                        }}
+                      >
+                        Bloqueio
+                      </Text>
+                      <Entypo
+                        name={IsBlockIcon}
+                        size={60}
+                        color={IsBlockColor}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.ItensPainel}
+                      onPress={() => Ble(IsBleColor)}
+                    >
+                      <Text
+                        style={{
+                          fontWeight: 'bold',
+                          fontSize: 17,
+                          color: IsBleColor,
+                        }}
+                      >
+                        Bluetooth
+                      </Text>
+                      <Feather name="bluetooth" size={60} color={IsBleColor} />
+                    </TouchableOpacity>
                   </View>
-                  <View style={styles.containerbattery}>
-                    <Ionicons
-                      name="speedometer-outline"
-                      size={50}
-                      color="#0D9488"
-                    />
+                </View>
+                <View style={styles.containerodometro}>
+                  <MaterialCommunityIcons
+                    name="bike-fast"
+                    size={70}
+                    color="#0D9488"
+                  />
+                  <View>
                     {bikes ? (
                       <View>
                         {bikes.status.map((statusbike, index) => (
-                          <Text key={index} style={styles.textbikeinfo}>
-                            {statusbike.battery}
-                          </Text>
+                          <AnimateNumber
+                            value={statusbike.totalOdometer}
+                            key={index}
+                            style={{
+                              fontSize: 60,
+                              fontWeight: 'bold',
+                              left: '10%',
+                            }}
+                            formatter={(val) => {
+                              return parseFloat(val).toFixed(10)
+                            }}
+                          />
                         ))}
                       </View>
                     ) : (
                       <ActivityIndicator size="large" color="#0D9488" />
                     )}
-
-                    <Text style={{ fontSize: 23, left: 0, top: 3 }}> Km</Text>
                   </View>
                 </View>
               </View>
-            </View>
+            ) : (
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: 5,
+                    backgroundColor: '#0D9488',
+                    borderRadius: 20,
+                  }}
+                  onPress={toggleModal}
+                >
+                  <MaterialCommunityIcons
+                    name="bike-fast"
+                    size={100}
+                    color="white"
+                  />
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      width: '70%',
+                      left: '10%',
+                      color: 'white',
+                    }}
+                  >
+                    Ola, você não possui bike cadastrada, aperte aqui e adicione
+                    a sua bike !
+                  </Text>
+                </TouchableOpacity>
+                <Modal isVisible={isModalVisible}>
+                  <View style={styles.containermodalbike}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'flex-start',
+                        alignItems: 'center',
+                        padding: 10,
+                      }}
+                    >
+                      <TouchableOpacity
+                        onPress={toggleModal}
+                        style={{ padding: 0 }}
+                      >
+                        <Feather name="arrow-left" size={30} color="black" />
+                      </TouchableOpacity>
+                    </View>
+                    <View
+                      style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 20,
+                          fontWeight: 'bold',
+                          padding: 10,
+                        }}
+                      >
+                        Informe o número de série da sua bike
+                      </Text>
+                      <TextInput
+                        style={styles.inputid}
+                        placeholder="Número de série"
+                      />
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                          justifyContent: 'space-evenly',
+                          width: '80%',
+                          top: 10,
+                          padding: 10,
+                        }}
+                      >
+                        <TouchableOpacity style={styles.buttonmodel}>
+                          <Text style={styles.textbuttonmodel}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonmodel}>
+                          <Text style={styles.textbuttonmodel}>Adicionar</Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
+                </Modal>
+              </View>
+            )}
           </View>
         </View>
       ) : (
@@ -254,6 +511,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginLeft: '5%',
   },
+  containerodometro: {
+    flexDirection: 'row', // Isso alinha os elementos horizontalmente
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+    height: '17%',
+    top: 10,
+    left: 8,
+  },
+  containerPainel: {
+    flexDirection: 'column',
+    width: '100%',
+    height: '20%',
+    top: '2%',
+  },
+  containerPainelItens: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  containermodalbike: {
+    height: 230,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  ItensPainel: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0D9488',
+    margin: 6,
+  },
 
   buttonsettings: {},
   buttonprofile: {},
@@ -278,5 +570,25 @@ const styles = StyleSheet.create({
   imagenbike: {
     width: '50%',
     height: '100%',
+  },
+  inputid: {
+    borderWidth: 1,
+    backgroundColor: '#F8F8F8',
+    borderRadius: 5,
+    padding: 1,
+    height: 40,
+    width: '90%',
+    fontSize: 18,
+    fontWeight: '300',
+  },
+  buttonmodel: {
+    backgroundColor: '#0D9488',
+    padding: 10,
+    borderRadius: 10,
+  },
+  textbuttonmodel: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
   },
 })
